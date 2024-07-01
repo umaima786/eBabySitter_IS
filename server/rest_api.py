31a0f33b17c845f797e72be5189c8a7bb620749c
Aown Raza <Aown.Raza@didactic-innovations.de>
2024-07-01 20:15:11 +0200

    Revert "."
    This reverts commit c7595fd0b67f266cef4d750c77f1e256398dfec5.

--- server/rest_api.py
+++ server/rest_api.py
@@ -11,7 +11,6 @@ from werkzeug.utils import secure_filename
 import threading
 import queue
 import time
-import numpy as np
 
 app = Flask(__name__)
 CORS(app)
@@ -54,32 +53,26 @@ def capture_frames():
     while True:
         if show_camera and picam2 is not None:
             frame = picam2.capture_array()
-            if frame is not None and isinstance(frame, np.ndarray):
+            if frame is not None:
                 frame_queue.put(frame)
-            else:
-                print("Captured frame is not valid")
 
 def process_frames():
     while True:
         if not frame_queue.empty():
             frame = frame_queue.get()
-            if isinstance(frame, np.ndarray):
-                bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                gray = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+            bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            gray = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-                if len(faces) == 0:
-                    emit_queue.put(('no_face_detected', {'message': 'No face detected'}))
+            if len(faces) == 0:
+                emit_queue.put(('no_face_detected', {'message': 'No face detected'}))
 
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(bgr_frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(bgr_frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-                ret, jpeg = cv2.imencode('.jpg', bgr_frame)
-                if ret:
-                    frame_bytes = jpeg.tobytes()
-                    frame_queue.put(frame_bytes)
-            else:
-                print("Processed frame is not valid")
+            ret, jpeg = cv2.imencode('.jpg', bgr_frame)
+            frame_bytes = jpeg.tobytes()
+            frame_queue.put(frame_bytes)
 
 def emit_events():
     while True:
